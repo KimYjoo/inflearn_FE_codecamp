332e4476dae937a90f15e061792b6ec4ecbe3c2f
Yjoo0726 <pas4976@gmail.com>
2025-10-01 13:03:37 +0900

    chore: section 08 quiz

---
+++ codecamp-classquiz/classquiz/pages/06/boards/postProduct/index.js
@@ -0,0 +1,19 @@
+export default function postProductPage() {
+    // const [seller]
+    return(
+       <>
+            <div>
+                판매자<input type="text" />
+            </div>
+            <div>
+                상품명<input type="text" />
+            </div>
+            <div>
+                상품내용<input type="text" />
+            </div>
+            <div>
+                상품가격<input type="text" />
+            </div>
+       </>
+    )
+}
