2d56acc4c135043cb473fb3007e6cdc376e7a453
Yjoo0726 <pas4976@gmail.com>
2025-10-01 12:58:10 +0900

    chore: section 08 class

--- codecamp-frontend-김용주/class/pages/_app.js
+++ codecamp-frontend-김용주/class/pages/_app.js
@@ -1,15 +1,14 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-
 export default function App({ Component }) {
 
   const client = new ApolloClient({
     uri: "http://backend-example.codebootcamp.co.kr/graphql",
     cache: new InMemoryCache() // 컴퓨터의 메모리에 백앤드에서 받아온 데이터 임시로 저장해놓기
   })
-
   return (
     <ApolloProvider client={client}>
+      
       <Component/>
     </ApolloProvider>
   ) 
--- codecamp-frontend-김용주/class/pages/section04/04-04-graphql-mutation-input/index.js
+++ codecamp-frontend-김용주/class/pages/section04/04-04-graphql-mutation-input/index.js
@@ -12,8 +12,11 @@ const 나의그래프큐엘셋팅 = gql`
 `
 
 export default function GraphqlMutationPage() {
-    const [나의함수] = useMutation(나의그래프큐엘셋팅)
-
+    const [나의함수] = useMutation(나의그래프큐엘셋팅);
+    const [writer, setWriter] = useState();
+    const [title, setTitle] = useState();
+    const [contents, setContents] = useState();
+    
     const onClickSubmit = async () => {
         const result = await 나의함수({
         variables: {
@@ -25,5 +28,22 @@ export default function GraphqlMutationPage() {
         console.log(result)
     }
 
-    return <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>
+    const onChangeWriter = (event) => {
+        setWriter(event.target.value);
+    }
+    const onChangeTitle = (event) => {
+        setTitle(event.target.value);
+    }
+    const onChangeContents = (event) => {
+        setContents(event.target.value);
+    }
+
+    return (
+        <div>
+            작성자: <input type="text" onChange={onChangeWriter}/>
+            제목: <input type="text" onChange={onChangeTitle}/>
+            내용: <input type="text" onChange={onChangeContents}/>
+            <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>
+        </div>
+    )
 }
