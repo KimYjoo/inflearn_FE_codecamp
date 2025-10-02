36bd0b9fc9ba9bc27c052878336b0bc6c62e07f3
Yjoo0726 <pas4976@gmail.com>
2025-10-02 13:23:24 +0900

    chore: section 7 setstate

---
+++ codecamp-frontend-김용주/class/pages/section07/07-01-set-state/index.js
@@ -0,0 +1,12 @@
+import BoardWrite from "/src/components/units/board/07-write/BoardWrite.container";
+
+export default function GraphqlMutationPage(props){
+
+    return(
+        <div>
+            <div>##여기는 페이지##</div>
+            <BoardWrite qqq="철수"/>
+            <div>##여기는 페이지##</div>
+        </div>
+    )
+}
